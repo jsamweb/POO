0208e3981806e4e260b54382e65f4498e9c6655d
jsamweb <jsam.web@gmail.com>
2021-08-10 00:04:41 +0200

    creando clases en los lenguajes

---
+++ POOUber/PHP/account.php
@@ -0,0 +1,8 @@
+<php?
+class account{
+    $id = Integer;
+    $name =   String;
+    $document = String;
+    $email = String;
+    $password = String;
+}
---
+++ POOUber/PHP/car.php
@@ -0,0 +1,7 @@
+<php?
+class car {
+$id = Integer;
+$license = String;
+$driver = String;
+$passengers = Integer;
+}
